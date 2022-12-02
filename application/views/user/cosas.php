7b06ad861c6cf25453c1552db580b74617f9ca0f
watefa1 <gaby2001@live.com.ar>
2022-12-02 16:57:43 +0000

    Mejora visual - Cosas

--- application/views/user/cosas.php
+++ application/views/user/cosas.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-
+		<link href="style.css" rel="stylesheet" type="text/css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <!-- Font Roboto CSS -->
@@ -14,14 +14,29 @@
 
     <title>Lista de cosas</title>
   </head>
-  <body background="Home.png">
+  <body>
 
     <div class="container">
 		<FONT COLOR="cyan"> 
         <h1 class="mt-5">Lista de cosas</h1>
         <div class="text-right">
-				<a href="<?php echo base_url(); ?>tags" class="btn btn-danger btn-outline-warning">Lista de tag</a> 
-        <a href="<?php echo base_url(); ?>agregar" class="btn btn-primary btn-outline-warning">Agregar cosa</a> 
+				<link href="boton.css" rel="stylesheet" type="text/css">	
+				<a href="<?php echo base_url(); ?>tags" class="btn btn-dark btn-outline-primary">
+  
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+   Tags
+  </a>
+	<a href="<?php echo base_url(); ?>agregar" class="btn btn-dark btn-outline-primary">
+  
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+   Agregar
+  </a>
 		</FONT>
         </div>
         <table class="table table-dark">
