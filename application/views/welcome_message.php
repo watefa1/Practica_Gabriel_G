ba40383fe845a0d265d3e5ff14e6ec60992a6d57
watefa1 <gaby2001@live.com.ar>
2022-12-15 17:42:24 +0000

    Diseño pagina welcome

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,17 +1,30 @@
 <!doctype html>
 <html lang="en">
   <head>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+		
     <title>gguevara practica</title>
   </head>
   <body background="fon1.png">
-  <a class="btn btn-lg btn-block btn-outline-warning" href="login" role="button">Ingresar</a>
+	<div class="container">
+		<FONT COLOR="cyan"> 
+        <h1 class="neon">Bienvenido</h1>
+        <div class="text-right">
+				<link href="boton.css" rel="stylesheet" type="text/css">	
+				<a href="login" class="btn btn-lg btn-block btn-outline-warning" role="button">
+				<span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+		  	Ingresar </a>
+  
     <div class="container">
 	  <div class="row">
 	  </div>
