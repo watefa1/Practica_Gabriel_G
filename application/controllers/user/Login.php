1843da9a2af8f508e5ce5a7230b8323d2d6ed09d
watefa1 <gaby2001@live.com.ar>
2022-12-15 17:14:08 +0000

    Arreglo la vista de login

--- application/controllers/user/Login.php
+++ application/controllers/user/Login.php
@@ -3,21 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+		$this->load->library(['form_validation','session']);
+		$this->load->database();
 	}
-	
 	public function index()	
 	{
-		if(isset($_POST['password'])){
-		$this->load->model('Usuario_model');
-		if($this->usuario_model->login($_POST['username'],$_POST['password'])){
-			redirect('user/cosas');
-		} else {
-			redirect('user/login');
-		}
-		}
 		$this->load->view("user/login");
 	}	
 }
